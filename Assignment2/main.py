73b8ea1a4f8d1dd7e110595e2aae0b88c3311fa9
Dan-Ial <danoh0911@gmail.com>
2021-02-05 15:13:45 -0500

    done contigs, pre comments

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -5,7 +5,9 @@ By: Evelyn Yach (20071956) & Daniel Oh (20063998)
 """
 import random
 
-
+##################
+# EULERIAN CYCLE #
+##################
 def eulerian_cycle(graph, random_start=True, choice_start=0):
     """
     generates a string which denotes a Eulerian cycle within the graph
@@ -100,7 +102,96 @@ def test_eulerian_cycle_empty():
     print()
 
 
-if __name__ == '__main__':
+def eulerian_cycle_main():
     test_eulerian_cycle_positive()
     test_eulerian_cycle_negative()
     test_eulerian_cycle_empty()
+
+
+###########
+# CONTIGS #
+###########
+def contigs(input_data):
+    """
+    generates a list of contigs from a list of input kmers
+    :param input_data: A list of kmer strings
+    :return: List of contigs found from the kmers
+    """
+
+    graph, contigs_list, to_remove_list = {}, [], []
+
+    # set up the graph
+    for kmer in input_data:
+        if kmer[:len(kmer)-1] not in graph:
+            graph[kmer[:len(kmer)-1]] = []
+        graph[kmer[:len(kmer)-1]].append(kmer[1:])
+
+    for vertex in graph:
+
+        edges_into_vertex, edges_out_of_vertex = num_of_inputs(vertex, graph), num_of_outputs(vertex, graph)
+
+        if edges_into_vertex != 1 or edges_out_of_vertex != 1:
+            # if there are more than 1 vertices that come after our current vertex
+            if edges_out_of_vertex > 0:
+
+                for vertex_prime in graph[vertex]:  # going through each vertex that branches out
+                    non_branching_path = vertex + vertex_prime[-1]
+                    # to_remove_list.append(vertex)  # remove vertex
+
+                    # while we're still on a straight path
+                    while num_of_inputs(vertex_prime, graph) == 1 and num_of_outputs(vertex_prime, graph) == 1:
+
+
+                        # for vertex_prime_prime in graph[vertex_prime]:
+                        non_branching_path = non_branching_path + graph[vertex_prime][0][-1]
+                        # to_remove_list.append(vertex_prime)
+                        vertex_prime = graph[vertex_prime][0]
+
+                    contigs_list.append(non_branching_path)
+
+    # for to_remove in to_remove_list:
+    #     if to_remove in graph:
+    #         del graph[to_remove]
+
+    return contigs_list
+
+
+def num_of_inputs(vertex, graph):
+    """
+    returns a count of the number of edges/inputs that are going into the vertex
+    :param vertex: the vertex we are concerned about
+    :param graph: the graph that :vertex: is within
+    :return: the number of edges entering the vertex
+    """
+    count = 0
+    # find every instance of vertex in the outputs of other vertices
+    for i in graph:
+        if vertex in graph[i]:
+            count += 1
+
+    return count
+
+
+def num_of_outputs(vertex, graph):
+    """
+    returns a count of the number of edges/inputs that are leaving the vertex
+    :param vertex: the vertex we are concerned about
+    :param graph: the graph that :vertex: is within
+    :return: the number of edges leaving the vertex
+    """
+    if vertex in graph:
+        # simply count
+        return len(graph[vertex])
+
+    return 0
+
+
+def contig_main():
+    rosalind_data = ['ATG', 'ATG', 'TGT', 'TGG', 'CAT', 'GGA', 'GAT', 'AGA']
+    output = contigs(rosalind_data)
+    print(output)
+
+
+if __name__ == '__main__':
+    # eulerian_cycle_main()
+    contig_main()
